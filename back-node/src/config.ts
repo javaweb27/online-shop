ee0ea65ce829976a872d355524e6bcfae02223da
javier <devjavicont@gmail.com>
2023-04-24 15:30:51 -0400

    back-node: add env vars for email and password of nodemailer

--- back-node/src/config.ts
+++ back-node/src/config.ts
@@ -7,3 +7,6 @@ export const PORT = process.env.PORT || "3060"
 export const JWT_TOKEN_KEY = process.env.JWT_TOKEN_KEY || "secret-key"
 /** link base of the client in github pages (frontend) */
 export const CORS_URL = process.env.CORS_URL || "http://localhost:5173"
+
+export const MAILER_EMAIL = process.env.MAILER_EMAIL || "your@email.com"
+export const MAILER_PASSWORD = process.env.MAILER_PASSWORD || "yourpassword"
