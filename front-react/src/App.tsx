20a5c8f52b37e4d870ddb58ca46282a6b2acfe94
javier <devjavicont@gmail.com>
2023-05-06 11:40:05 -0400

    front-react: separate react query setup to the ReactQuerySetup component

--- front-react/src/App.tsx
+++ front-react/src/App.tsx
@@ -2,28 +2,21 @@ import "./styles/index.css"
 import "./styles/App.css"
 import "./styles/tailwind.css"
 import { RoutesConfig } from "./RoutesConfig"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { themeColors } from "./styles/themeColors"
 import { Global as GlobalCss } from "@emotion/react"
 import { ReduxSetup } from "./ReduxSetup"
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: { networkMode: "always" },
-    mutations: { networkMode: "always" },
-  },
-})
+import { ReactQuerySetup } from "./ReactQuerySetup"
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
+    <ReactQuerySetup>
       <ReduxSetup>
         <RoutesConfig />
         <ReactQueryDevtools />
         <GlobalCss styles={themeColors} />
       </ReduxSetup>
-    </QueryClientProvider>
+    </ReactQuerySetup>
   )
 }
 
---
+++ front-react/src/ReactQuerySetup.tsx
@@ -0,0 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { networkMode: "always" },
+    mutations: { networkMode: "always" },
+  },
+})
+
+export const ReactQuerySetup = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  )
+}
