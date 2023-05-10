d7f95ef622a34fb64bc2099d6b49534351ad9f29
javier <devjavicont@gmail.com>
2023-05-10 14:06:30 -0400

    back-node: register controller (auth) no longer uses the auth token to confirm the email, and no longer sends the new user data in the response

--- back-node/src/folders/auth/controllers/register.controller.ts
+++ back-node/src/folders/auth/controllers/register.controller.ts
@@ -1,42 +1,48 @@
 import { Request, Response } from "express"
-import { jwtSigner } from "../../../lib/jwtSigner"
 import UserModel from "../../users/UserModel"
 import { mailerSendConfimationLink } from "../../../mailer"
+import { jwtConfirmEmailSigner } from "../../../lib/jwtConfirmEmailSigner"
 
-export const register = async (cli: Request, ser: Response) => {
+export const register = async (cli: Request, res: Response) => {
   console.log(`POST /users - create one new user`)
 
   let confirmationToken: string
-
-  try {
-    confirmationToken = await jwtSigner({ email: cli.body.email })
-  } catch (error) {
-    ser.status(500).json({ message: "error when create the token to confirm user email" })
-    return
-  }
 
   try {
     const encryptedPassword = await UserModel.schema.methods.encryptPassword(
       cli.body.password
     )
 
-    const user = await UserModel.create({
+    const user = new UserModel({
       email: cli.body.email,
       password: encryptedPassword,
       balance: 270,
       orders: [],
       isEmailConfirmed: false,
-      confirmationToken,
+      confirmationToken: "replace-before-saving",
     })
+
+    try {
+      confirmationToken = await jwtConfirmEmailSigner(user._id.toString())
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "error when create the token to confirm user email" })
+      return
+    }
+
+    user.confirmationToken = confirmationToken
+
+    await user.save()
 
     mailerSendConfimationLink({
       userConfirmationToken: confirmationToken,
       userEmail: user.email,
     })
 
-    ser.status(201).json(user)
+    res.status(201).json({ message: "user registered" })
   } catch (error: any) {
     console.error("Cannot register user, fields are invalid, " + error.message)
-    ser.status(400).json({ message: "Cannot register user, fields are invalid" })
+    res.status(400).json({ message: "Cannot register user, fields are invalid" })
   }
 }
