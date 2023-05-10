f530c98ad662f04080e75e3d2194bb6bb12c7cf9
javier <devjavicont@gmail.com>
2023-05-10 13:45:44 -0400

    back-node: create a new environment varible for sending tokens to confirm user email

--- back-node/src/config.ts
+++ back-node/src/config.ts
@@ -4,7 +4,14 @@ config()
 export const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost:27017/test-online-shop"
 export const PORT = process.env.PORT || "3060"
+
+/** used only for auth */
 export const JWT_TOKEN_KEY = process.env.JWT_TOKEN_KEY || "secret-key"
+
+/** used only for email confirmation, not for auth */
+export const JWT_CONFIRM_EMAIL_KEY =
+  process.env.JWT_CONFIRM_EMAIL_KEY || "secret-email-key"
+
 /** link base of the client in github pages (frontend) */
 export const CORS_URL = process.env.CORS_URL || "http://localhost:5173"
 
