f1a733d4d6b33952d0a28847874ef92dd77357f0
javier <devjavicont@gmail.com>
2024-07-06 13:57:24 -0400

    back-node: Product: create ProductService for the database

---
+++ back-node/src/folders/products/ProductService.ts
@@ -0,0 +1,22 @@
+import ProductModel from "./ProductModel"
+
+export class ProductService {
+  async create(product: { title: string; category: string; imgSrc: string }) {
+    const createdProduct = await ProductModel.create({
+      title: product.title,
+      price: Math.trunc(Math.random() * 100) + 1,
+      category: product.category,
+      imgSrc: product.imgSrc,
+    })
+
+    return createdProduct
+  }
+  async getAll(filters: { category: string } | Record<string, never>) {
+    const products = await ProductModel.find(filters)
+
+    return products
+  }
+  async getById(filters: { id: string }) {
+    return await ProductModel.findById(filters.id)
+  }
+}
