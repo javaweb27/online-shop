c5e3d7db8b5db608099eabf9b60d27f048f1dfb2
javier <devjavicont@gmail.com>
2024-07-06 14:33:32 -0400

    back-node: use morgan

--- back-node/src/app.ts
+++ back-node/src/app.ts
@@ -1,9 +1,11 @@
 import express from "express"
 import cors from "cors"
 import { CORS_URL } from "./config"
+import morgan from "morgan"
 
 const app = express()
 
+app.use(morgan("dev"))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cors({ origin: [CORS_URL] }))
