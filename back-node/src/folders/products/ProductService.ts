fd521037ade939b5187e068413bb3cdbac97303e
javier <devjavicont@gmail.com>
2024-07-06 18:53:37 -0400

    back-node: ProductService: throw boom exeptions for responses that are not ok

--- back-node/src/folders/products/ProductService.ts
+++ back-node/src/folders/products/ProductService.ts
@@ -1,7 +1,13 @@
-import ProductModel from "./ProductModel.js"
+import ProductModel, { PRODUCTS_CAT } from "./ProductModel.js"
+import boom from "@hapi/boom"
 
 export class ProductService {
   async create(product: { title: string; category: string; imgSrc: string }) {
+    // @ts-ignore
+    if (!PRODUCTS_CAT[product.category]) {
+      throw boom.badRequest("this category does not exist")
+    }
+
     const createdProduct = await ProductModel.create({
       title: product.title,
       price: Math.trunc(Math.random() * 100) + 1,
@@ -14,9 +20,19 @@ export class ProductService {
   async getAll(filters: { category: string } | Record<string, never>) {
     const products = await ProductModel.find(filters)
 
+    if (0 == products.length) {
+      throw boom.notFound("products not found")
+    }
+
     return products
   }
   async getById(filters: { id: string }) {
-    return await ProductModel.findById(filters.id)
+    const product = await ProductModel.findById(filters.id)
+
+    if (!product) {
+      throw boom.notFound("product not found")
+    }
+
+    return product
   }
 }
